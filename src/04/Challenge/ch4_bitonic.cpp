6304fd64114357ffe409f27533bfcda9cc856572
Mo <72713002+MohamadSayegh@users.noreply.github.com>
2024-03-30 17:30:19 +0000

    challenge bitonic

--- src/04/Challenge/ch4_bitonic.cpp
+++ src/04/Challenge/ch4_bitonic.cpp
@@ -19,17 +19,37 @@
 //           v: A reference to the vector to analyze.
 // Returns: A boolean value: True for bitonic sequences, false otherwise.
 bool is_bitonic(const std::vector<int> &v){
-    
-    // Write your code here
 
-    return false;
+    // Iterator to keep track of the current position in the vector
+    std::vector<int>::const_iterator it = v.begin();
+
+    // While loop to iterate over the vector until the iterator reaches the end
+    int delta = 1, new_delta = 1;
+    int e1 =0,e2=0;
+    int change =0;
+    while (it != v.end()) {
+        e1 = *it;
+        e2 = *(it+1);
+        new_delta = e2 - e1;
+        if (delta*new_delta<0){
+            change++;
+        }
+        delta = new_delta;
+        ++it; // Move the iterator to the next element
+    }
+    std::cout<<change<<std::endl;
+    if (change <=2){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 // Main function
 int main(){
     // Uncomment one of these lines and make sure you get the result at the right. 
     
-    std::vector<int> myvec = {1, 2, 5, 4, 3};  // Yes
+    // std::vector<int> myvec = {1, 2, 5, 4, 3};  // Yes
     // std::vector<int> myvec = {1, 1, 1, 1, 1};  // Yes
     // std::vector<int> myvec = {3, 4, 5, 2, 2};  // Yes
     // std::vector<int> myvec = {3, 4, 5, 2, 4};  // No
